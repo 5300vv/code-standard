1ee60437ae034776dd10626864ca5f58e34672df
5300vv <530094021@qq.com>
2023-03-18 00:38:33 +0800

    feat:新增npm postcss-html

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,44 +1,40 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{ title }}</text>
-		</view>
-	</view>
+    <view class="content">
+        <image class="logo" src="/static/logo.png"></image>
+        <view class="text-area">
+            <text class="title">{{ title }}</text>
+        </view>
+    </view>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			title: 'Hello',
-		}
-	},
-	onLoad() {
-		console.log(111)
-	},
-	methods: {},
+    data() {
+        return {}
+    },
+    onLoad() {},
+    methods: {},
 }
 </script>
 
 <style lang="scss" scoped>
 .content {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 }
 .logo {
-	width: 200rpx;
-	height: 200rpx;
-	margin: 200rpx auto 50rpx;
+    width: 200rpx;
+    height: 200rpx;
+    margin: 200rpx auto 50rpx;
 }
 .text-area {
-	display: flex;
-	justify-content: center;
+    display: flex;
+    justify-content: center;
 }
 .title {
-	font-size: 36rpx;
-	color: #8f8f94;
+    font-size: 36rpx;
+    color: #8f8f94;
 }
 </style>
